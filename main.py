d6abdb505d57bc713c7a268d1929035313850c83
Michał Hewelt <mhewelt9@gmail.com>
2022-12-04 13:03:36 +0100

    set difficulty

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from art import logo
+import random
 
 print(logo)
+
+number = random.randint(1, 100)
+difficulty = input("Choose a difficulty. Type 'easy or 'hard': ").lower()
+if difficulty == 'hard':
+    lives = 5
+else:
+    lives = 10
+
+
+
